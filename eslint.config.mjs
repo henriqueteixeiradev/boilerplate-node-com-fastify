8e269b523f840caee89e130a49609c810fab0725
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2025-03-18 12:58:47 -0300

    asdas

---
+++ eslint.config.mjs
@@ -0,0 +1,127 @@
+import { defineConfig, globalIgnores } from "eslint/config"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import prettier from "eslint-plugin-prettier"
+import globals from "globals"
+import tsParser from "@typescript-eslint/parser"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default defineConfig([
+  globalIgnores([
+    "**/dist",
+    "**/node_modules",
+    "**/coverage",
+    "**/.js",
+    "**/.d.ts",
+  ]),
+  {
+    extends: compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+    ),
+
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "lf",
+        },
+      ],
+      "linebreak-style": ["error", "unix"],
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+          allowHigherOrderFunctions: true,
+        },
+      ],
+
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^",
+          varsIgnorePattern: "^",
+        },
+      ],
+
+      "no-unused-vars": "off",
+      "no-unused-imports": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: false,
+        },
+      ],
+
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+
+      "no-console": [
+        "warn",
+        {
+          allow: ["error", "warn", "info", "log"],
+        },
+      ],
+
+      "no-return-await": "error",
+      "require-await": "error",
+      "no-async-promise-executor": "error",
+      "no-promise-executor-return": "error",
+
+      "max-len": [
+        "error",
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+    },
+  },
+])
