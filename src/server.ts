da7d45612c9ca7513b34cd6c50a87a373ecd3943
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2025-03-18 13:05:09 -0300

    aSDASDA

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
-const name
+import express from "express";
+import { Request, Response } from "express";
+const port = 3333;
 
-console.log("asdsa")
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
